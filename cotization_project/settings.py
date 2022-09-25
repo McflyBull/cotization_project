b9f7b91aec414ef379d86bc18640f5737f0af6f3
McFly <55802619+McflyBull@users.noreply.github.com>
2022-09-25 15:18:01 -0300

    allowed host

--- cotization_project/settings.py
+++ cotization_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rh5!n@%e5^@zm)u(y2y0%e756!bev3^lo7bcxt$5m1nl2#1_-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.pythonanywhere.com']
 
 
 # Application definition
