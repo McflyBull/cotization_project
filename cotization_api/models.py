3ccded8632ca9e6bd52e1904c8466a0686d55ce3
McFly <55802619+McflyBull@users.noreply.github.com>
2022-10-02 12:44:42 -0300

    asd

--- cotization_api/models.py
+++ cotization_api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from datetime import datetime
 from django.utils.timezone import now
+from django.utils import timezone
 
 # Create your models here.
 
 class Dollar(models.Model):
    price = models.FloatField()
-   stamp = models.DateTimeField(auto_now_add=False)
+   stamp = models.DateTimeField(default=timezone.now)
