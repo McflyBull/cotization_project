9541ba475148ec4d98b32077dd0551e0b4b36419
McFly <55802619+McflyBull@users.noreply.github.com>
2022-10-01 23:20:53 -0300

    model clear

--- cotization_api/models.py
+++ cotization_api/models.py
@@ -5,16 +5,5 @@ from django.utils.timezone import now
 # Create your models here.
 
 class Dollar(models.Model):
-
-
-   #name = models.CharField(max_length=100)
-
-
-   #classification = models.CharField(max_length=100)
-
-
-   #language = models.CharField(max_length=100)
-
    price = models.FloatField()
-
    stamp = models.DateTimeField(default=datetime.now,)
